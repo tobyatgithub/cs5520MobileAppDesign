c31db43ad6dfbac587692a1c32ebecd02ff23154
tobyatgithub <toby.fangyuan@gmail.com>
2021-02-15 15:47:12 -0800

    create empty event project for cs5520. Preparing for assignment 1.

---
+++ NUMAD2021Spring-FangyuanHuang/app/src/main/java/edu/neu/madcourse/numad2021spring_fangyuanhuang/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad2021spring_fangyuanhuang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
