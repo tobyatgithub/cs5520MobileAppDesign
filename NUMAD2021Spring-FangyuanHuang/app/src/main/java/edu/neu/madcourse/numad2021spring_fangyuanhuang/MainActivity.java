c42c7ac0bc91f0f74cae3665fe31c8c973ae0e9c
tobyatgithub <toby.fangyuan@gmail.com>
2021-02-16 09:21:50 -0800

    checking ppoint... kind of working.

--- NUMAD2021Spring-FangyuanHuang/app/src/main/java/edu/neu/madcourse/numad2021spring_fangyuanhuang/MainActivity.java
+++ NUMAD2021Spring-FangyuanHuang/app/src/main/java/edu/neu/madcourse/numad2021spring_fangyuanhuang/MainActivity.java
@@ -3,6 +3,12 @@ package edu.neu.madcourse.numad2021spring_fangyuanhuang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void convertCurrency(View view) {
+        EditText dollarText = findViewById(R.id.dollarText);
+        TextView textView = findViewById(R.id.textView);
+
+        if (!dollarText.getText().toString().equals("")) {
+            float dollarValue = Float.valueOf(dollarText.getText().toString());
+            float euroValue = dollarValue * 0.85F;
+            textView.setText(
+                    String.format(Locale.getDefault(), "%.2f", euroValue));
+        } else {
+            textView.setText(R.string.no_value_string);
+        }
+    }
+
+    public void showAbout(View view) {
+        TextView AboutString = findViewById(R.id.textView);
+        AboutString.setText(R.string.my_info);
+    }
+
 }
