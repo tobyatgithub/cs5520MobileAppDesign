991c26d26717c6b782921b7a76633a755f610689
tobyatgithub <toby.fangyuan@gmail.com>
2021-03-02 14:40:12 -0800

    save point. need to replace ListView by RecycleView.

--- NUMAD2021Spring-FangyuanHuang/app/src/main/java/edu/neu/madcourse/numad2021spring_fangyuanhuang/LinkCollector.java
+++ NUMAD2021Spring-FangyuanHuang/app/src/main/java/edu/neu/madcourse/numad2021spring_fangyuanhuang/LinkCollector.java
@@ -1,17 +1,43 @@
 package edu.neu.madcourse.numad2021spring_fangyuanhuang;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class LinkCollector extends AppCompatActivity {
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    private ListView myListView;
     private FloatingActionButton fab;
+    private String m_Text = "";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myListView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        myListView.setAdapter(adapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +48,38 @@ public class LinkCollector extends AppCompatActivity {
         fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(LinkCollector.this, "Fab clicked", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                addListItem();
+                Snackbar.make(view, "Item added to list",
+                        Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
+    }
+
+    private void addListItem() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("title");
+
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_TEXT_VARIATION_URI);
+        input.setLinksClickable(true);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+                listItems.add(m_Text);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
     }
 }
